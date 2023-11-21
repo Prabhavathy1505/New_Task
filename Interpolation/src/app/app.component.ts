82eff064c3a0cbb0972903f752cf3803dc4f751b
prabhavathy <prabhavathyjothi15@gmail.com>
2023-11-21 14:03:05 +0530

    Interpolation

---
+++ Interpolation/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Interpolation';
+  FirstName: string = 'Roshini';
+  LastName: string = 'ArulKumar';//hard coded string
+ 
+    people = [
+      { FirstName: 'Vishal', LastName: 'Mohanty' },
+      {FirstName:  'Anurag',
+      LastName:'Mohanty'},
+      {FirstName:  'Karthik',
+      LastName:'Arulkumar'}
+    ]
+    Address : string = '';
+    GetFullName() : string{
+      return this.FirstName + ' ' + this.LastName;
+    }
+    ImagePath : string = "https://www.jquery-az.com/html/images/banana.jpg"
+}
