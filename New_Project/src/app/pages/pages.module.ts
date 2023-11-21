2181f26ccd9714641f607fdbbb142b149df38a6b
prabhavathy <prabhavathyjothi15@gmail.com>
2023-11-21 17:48:29 +0530

    new_changes for dyanamic login

--- New_Project/src/app/pages/pages.module.ts
+++ New_Project/src/app/pages/pages.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
